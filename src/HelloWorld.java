a418bca35d044548b2fde5a842ddaaccd7904921
Mary White <mary.a.white28@gmail.com>
2021-10-21 16:42:06 -0400

    completed exercise without bonus

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+    }
+}
