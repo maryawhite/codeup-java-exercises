1f382d61bbeef006d66dfc2b6ededda7fd3f123a
Mary White <mary.a.white28@gmail.com>
2021-10-22 15:33:50 -0400

    completed number 1

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,8 +3,8 @@ public class ConsoleExercises {
     public static void main(String[] args) {
 
         double pi = 3.14159;
-
-        System.out.printf("The value of pi is approximately " + pi);
+        //write some code so that pi comes out as 3.14
+        System.out.format("The value of pi is approximately %.2f", pi);
 
     }
 }
