1acd8cee9d1e57ef0b929e73d7d60fcbf0d91ce4
Mary White <mary.a.white28@gmail.com>
2021-10-22 15:11:04 -0400

    following along with lecture notes, using scanner, and nextLine

---
+++ src/lectureConsoleIO.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lectureConsoleIO {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = "Shanshan";
+//        System.out.printf("Hello there, %s. Nice to see you. \n", name);
+//
+//        String greeting = "Howdy";
+//        String cohortName = "Quasar";
+//
+//        System.out.printf("%s, %s", greeting, cohortName);
+
+        System.out.print("Enter Something: ");
+
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+    }
+}
