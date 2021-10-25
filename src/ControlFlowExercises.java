a6c79ac91ba0ae9871db8673344a6d67940bfc80
Mary White <mary.a.white28@gmail.com>
2021-10-25 12:41:19 -0400

    completed the next do while, count down by 5s from 100 to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,11 +9,17 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        int i = 0;
-        do {
-            i += 2;
+//        int i = 0;
+//        do {
+//            i += 2;
+//            System.out.println(i);
+//        } while (i < 100);
+
+        int i = 100;
+        do{
             System.out.println(i);
-        } while (i < 100);
+            i -= 5;
+        } while (i >= -10);
 
     }
 }
