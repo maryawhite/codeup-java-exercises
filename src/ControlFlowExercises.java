b59db93348f04681728b50c00a8b5a8e0f0d6494
Mary White <mary.a.white28@gmail.com>
2021-10-25 17:18:12 -0400

    added the prompt to continue for question 3.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -19,11 +19,11 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        } while (i < 100);
 
-//        int i = 100;
+//        int m = 100;
 //        do{
-//            System.out.println(i);
-//            i -= 5;
-//        } while (i >= -10);
+//            System.out.println(m);
+//            m -= 5;
+//        } while (m >= -10);
 
 //        long i = 2;   //this needs to be long, not int
 //        do{
@@ -35,8 +35,8 @@ public class ControlFlowExercises {
 //            System.out.print(i + " ");
 //        }
 //
-//        for(long i = 2; i < 1000000; i *= i){
-//            System.out.println(i);
+//        for(long n = 2; n < 1000000; n *= n){
+//            System.out.println(n);
 //        }
 
 //        for(int i = 0; i <= 100; i += 2){
@@ -54,6 +54,10 @@ public class ControlFlowExercises {
 //            }
 //        }
 
+        //consider using a variable like boolean divBy3 = (i % 3) == 0;
+
+        boolean confirm1;
+        do {
         System.out.print("What number would you like to go up to? ");
 
         int output = scanner.nextInt();
@@ -61,40 +65,42 @@ public class ControlFlowExercises {
         System.out.println("Here is your table!");
         System.out.println("number | squared | cubed");
         System.out.println("------ | ------- | -----");
-        String pipe = "|";
 
-        for(int i = 1; i <= output; i++){
-            System.out.printf("%-7d", i);
-            System.out.printf("%s", pipe);
-            System.out.printf("%-7d", i*i);
-            System.out.printf("%s", pipe);
-            System.out.printf("%-7d%n", i*i*i);
-        }
 
-        String userInput ="";
-        do {
-            System.out.print("Enter a numerical grade from 0-100. ");
-
-            int grade = scanner.nextInt();
-
-            if (grade <= 100 && grade >= 88) {
-                System.out.println("Your grade is an A");
-            } else if (grade <= 87 && grade >= 80) {
-                System.out.println("Your grade is a B");
-            } else if (grade <= 79 && grade >= 67) {
-                System.out.println("Your grade is a C");
-            } else if (grade <= 66 && grade >= 60) {
-                System.out.println("Your grade is a D");
-            } else if (grade <= 59 && grade >= 0) {
-                System.out.println("Your grade is a F");
-            } else {
-                System.out.println("Sorry, invalid input");
+            for (int i = 1; i <= output; i++) {
+                System.out.printf("%-7d| %-9d| %-6d\n", i, i * i, i * i * i);
+//            System.out.printf("%-7d", i*i);
+//            System.out.printf("%-7d%n", i*i*i);
             }
+            System.out.println("Continue? [Y/N] ");
+            confirm1 = scanner.next().equalsIgnoreCase("y");
+        } while(confirm1);
 
-            System.out.println("Continue? [y/N] ");
-            userInput = scanner.next();
-//            boolean confirmation = userInput.equals("y");
-        } while(userInput.equals("Y") || userInput.equals("y"));
+//        String userInput ="";
+//        do {
+//            System.out.print("Enter a numerical grade from 0-100. ");
+//
+//            int grade = scanner.nextInt();
+//
+//            if (grade <= 100 && grade >= 88) {
+//                System.out.println("Your grade is an A");
+//            } else if (grade <= 87 && grade >= 80) {
+//                System.out.println("Your grade is a B");
+//            } else if (grade <= 79 && grade >= 67) {
+//                System.out.println("Your grade is a C");
+//            } else if (grade <= 66 && grade >= 60) {
+//                System.out.println("Your grade is a D");
+//            } else if (grade <= 59 && grade >= 0) {
+//                System.out.println("Your grade is a F");
+//            } else {
+//                System.out.println("Sorry, invalid input");
+//            }
+//
+//            System.out.println("Continue? [Y/N] ");
+//            userInput = scanner.next();
+//        } while(userInput.equals("Y") || userInput.equals("y"));
+
+        scanner.close();
 
     }
 }
