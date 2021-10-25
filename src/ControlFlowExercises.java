1dfa65fb1847b9315a94cc2199d80e94483025a9
Mary White <mary.a.white28@gmail.com>
2021-10-25 12:37:02 -0400

    completed do while to count by 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,18 @@ public class ControlFlowExercises {
 
     public static void main(String[] args){
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
 
-        while (i <= 15){
-            System.out.print(i + " ");
-            i++;
-        }
+        int i = 0;
+        do {
+            i += 2;
+            System.out.println(i);
+        } while (i < 100);
+
     }
 }
