a7e4e3e9eb4b562ee283b9a428968bb6bf23c872
Mary White <mary.a.white28@gmail.com>
2021-10-25 13:32:45 -0400

    completed fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -35,8 +35,19 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for(int i = 0; i <= 100; i += 2){
-            System.out.print(i + " ");
+//        for(int i = 0; i <= 100; i += 2){
+//            System.out.print(i + " ");
+//        }
+        for(int i = 1; i <=100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i + " ");
+            }
         }
 
     }
