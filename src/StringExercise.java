0f64bd83756a28fc86aebf4d2dfa6589c1ec8bb7
Mary White <mary.a.white28@gmail.com>
2021-10-26 11:43:04 -0400

    completed number 1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,5 +2,17 @@ public class StringExercise {
 
     public static void main(String[] args) {
 
+        String lyrics = "We don't need no education\n" + "We don't need no thought control";
+        System.out.println(lyrics);
+
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+
+        String mainDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(mainDrive);
+
+        String slashes = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
+        System.out.println(slashes);
+
     }
 }
