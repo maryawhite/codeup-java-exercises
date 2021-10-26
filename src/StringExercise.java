94b2e8d1d5a57a9f24f629eb0448274ab1013f92
Mary White <mary.a.white28@gmail.com>
2021-10-26 14:39:02 -0400

    took out my extra strings

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,18 +1,18 @@
 public class StringExercise {
 
     public static void main(String[] args) {
+        String aString;
+        aString = "We don't need no education\n" + "We don't need no thought control";
+        System.out.println(aString);
 
-        String lyrics = "We don't need no education\n" + "We don't need no thought control";
-        System.out.println(lyrics);
+        aString = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(aString);
 
-        String check = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(check);
+        aString = "In windows, the main drive is usually C:\\";
+        System.out.println(aString);
 
-        String mainDrive = "In windows, the main drive is usually C:\\";
-        System.out.println(mainDrive);
-
-        String slashes = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
-        System.out.println(slashes);
+        aString = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
+        System.out.println(aString);
 
     }
 }
