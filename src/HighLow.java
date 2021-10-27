94dbd083219f42ad4a718ead7ac78713ba6ee9f7
Mary White <mary.a.white28@gmail.com>
2021-10-27 17:22:04 -0400

    generate a random number, user the getInteger method to prompt for a number, now need to add a loop to keep prompting until they guess correctly

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,6 +1,24 @@
+import java.util.Random;
+
 public class HighLow {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        int result = random.nextInt(101);  //generate a number from 0 to 100
+        result = result + 1;
+
+//        System.out.println("The random number is: " + result);
+
+        int userGuess = MethodsExercises.getInteger(0,100);  //this pulls my method from another file and asks the user to enter a number from 1-100
+
+        if(userGuess == result){
+            System.out.println("CONGRATS!!! that's correct");
+        } else if(userGuess > result){
+            System.out.println("LOWER!");
+        } else if (userGuess < result){
+            System.out.println("HIGHER!");
+        }
     }
 }
