39754764a7146b7419e979ec92db4b6a8661b307
Mary White <mary.a.white28@gmail.com>
2021-10-26 22:06:41 -0400

    completed number 2 enter a number between 1 and 10

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static int add(int num1, int num2){
@@ -12,6 +14,14 @@ public class MethodsExercises {
         return num1 * num2;
     }
 
+    public static int multiplyLoop(int num1, int num2){
+        int result = 0;
+        for(int i = 0; i < num1; i++){
+            result += num2;     //this works bc multiplication is the same as adding the first number over and over the number of the 2nd number
+        }
+        return result;
+    }
+
     public static int divide(int num1, int num2){
         return num1 / num2;
     }
@@ -20,6 +30,18 @@ public class MethodsExercises {
         return num1 % num2;
     }
 
+    public static int getInteger(int min, int max){
+        int userNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10 ");
+        userNumber = scanner.nextInt();
+        if(userNumber <= 10 && userNumber >= 1){
+            return userNumber;
+        } else {
+            userNumber = getInteger(1, 10);
+        }
+        return userNumber;
+    }
 
 
     public static void main(String[] args) {
@@ -27,7 +49,9 @@ public class MethodsExercises {
         System.out.println("subtract: " + subtract(15, 3));
         System.out.println("divide: " + divide(15, 3));
         System.out.println("multiply: " + multiply(15, 3));
+        System.out.println("multiplyLoop: " + multiplyLoop(15, 3));
         System.out.println("remainder: " + remainder(15, 3));
+        getInteger(1, 10);
     }
 
     }
