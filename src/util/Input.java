02aef6e8d13c0cce9cf87cc8b83d5ba9ab573b96
Mary White <mary.a.white28@gmail.com>
2021-10-28 16:25:01 -0400

    creating a package named util with a class and methods

---
+++ src/util/Input.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner scanner;
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString(){
+//        System.out.print("Enter a string");
+        return this.scanner.nextLine();
+    }
+
+    public boolean yesNo(){
+//        System.out.println("Enter y for yes, n for no");
+        return this.scanner.nextLine().equalsIgnoreCase("y") || this.scanner.nextLine().equalsIgnoreCase("yes");
+    }
+
+    public int getInt(int min, int max){
+        int userNumber;
+        userNumber = this.scanner.nextInt();
+        if(userNumber <= max && userNumber >= min){
+            return userNumber;
+        } else {
+            return getInt(min, max);
+        }
+    }
+
+    public int getInt(){
+        return this.scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max){
+        double userNumber;
+        userNumber = this.scanner.nextDouble();
+        if(userNumber <= max && userNumber >= min){
+            return userNumber;
+        } else {
+            return getDouble(min, max);
+        }
+    }
+
+    public double getDouble(){
+        return this.scanner.nextDouble();
+    }
+
+}
