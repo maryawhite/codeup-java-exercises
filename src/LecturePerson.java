887dcd2fbfed50ea3803838630a29742c6403fb9
Mary White <mary.a.white28@gmail.com>
2021-10-28 14:14:34 -0400

    following along with lecture

--- src/LecturePerson.java
+++ src/LecturePerson.java
@@ -4,9 +4,13 @@ public class LecturePerson {
     public String lastName;
     public int age;
 
+    //instance method
     public String joinChat(){
         return firstName + " has joined the chat!";
     }
+
+    //make a static property, you can use underscores in place of commas for big numbers
+    public static long worldPop = 7_9_000_000L;
 
     public static void main(String[] args) {
         LecturePerson mary = new LecturePerson();
@@ -18,6 +22,20 @@ public class LecturePerson {
         System.out.printf("%s %s is my name!%n", mary.firstName, mary.lastName);
 
         System.out.println(mary.joinChat());
+
+        System.out.println("worldPop " + worldPop);
+
+        LecturePerson douglas = new LecturePerson();
+        douglas.firstName = "Douglas";
+        douglas.lastName = "Hirsh";
+
+        //how to add douglas into our count of worldPop?
+        LecturePerson.worldPop += 1;
+
+        System.out.println("worldPop with douglas = " + worldPop);
+        System.out.println("LecturePerson.worldPop = " + LecturePerson.worldPop);
+
+        //can we talk to person.firstName? No, non-static variable cannot be access from a static context
 
     }
 
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    public String name;
+    public String cohort;
+    private double grade;
+
+    //use method overloading
+    public Student(String studentName){
+//        name = studentName;
+//        cohort = "Unassigned";
+//        this.name = studentName; same thing using this
+//        this.cohort = "Unassigned";
+
+        //calling constructor inside of constructor
+        this(studentName, "unassigned");
+    }
+
+    public Student(String studentName, String studentCohort){
+//        name = studentName;
+//        cohort = studentCohort;
+        //or when using this
+        this.name = studentName;
+        this.cohort = studentCohort;
+    }
+
+    public Student(String studentName, String studentCohort, double grade){
+        this.name = studentName;
+        this.cohort = studentCohort;
+        this.grade = grade;
+    }
+
+    public static void main(String[] args){
+        //before constructors: use .notation like the LecturePerson example
+        //now
+        //first constructor
+        Student shanshan = new Student("Shanshan S.");
+
+        System.out.println("shanshan.name = " + shanshan.name + " cohort " + shanshan.cohort);
+
+        //second constructor
+        Student kenneth = new Student("Kenneth H.", "Deimos");
+
+        System.out.printf("%s is the student name and %s is their cohort", kenneth.name, kenneth.cohort);
+
+        Student mcKen = new Student("mcKen", "mcDeimos", 88.8);
+        System.out.println("mcKen.grade = " + mcKen.grade);
+        //his grade will show up within this class. But, if you create a new class and try to bring it in you won't be able to access it
+
+    }
+
 }
