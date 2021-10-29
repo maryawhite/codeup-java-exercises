490736a23d5cd122abef86233e86f8490bfcfe19
Mary White <mary.a.white28@gmail.com>
2021-10-29 09:25:04 -0400

    initial file creation

---
+++ src/shapes/Circle.java
@@ -0,0 +1,12 @@
+package shapes;
+
+public class Circle {
+
+    //create a constructor, the constructor has the same name as the class.
+    //This class should have a private radius property that is set through the constructor
+
+
+
+
+
+}
