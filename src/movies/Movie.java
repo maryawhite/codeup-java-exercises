346ae8420c7d134dc7cddaf0a0b14dc8f2d700de
Mary White <mary.a.white28@gmail.com>
2021-11-01 16:43:55 -0400

    working on exercise 3, Movies

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,34 @@
-package movies;public class Movie {
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    //create a constructor, the constructor has the same name as the class.
+    public Movie(String movieName, String movieCategory){
+        this.name = movieName;
+        this.category = movieCategory;
+    }
+
+    //Create methods to access these properties and change them (getters and setters).
+    public String getMovieInfo(String name){
+        return "name: " + name;
+    }
+
+    public String getMovieInfo(String name, String category){
+        return "name: " + name + "category: " + category;
+    }
+
+    public void setMovieInfo(String movieName){
+        this.name = movieName;
+    }
+
+    public void setMovieInfo(String movieName, String movieCategory){
+        this.name = movieName;
+        this.category = movieCategory;
+    }
+
+
+
+
 }
