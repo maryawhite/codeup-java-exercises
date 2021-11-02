6f7467a4139d3ea941dfdcaff974bb8f69832181
Mary White <mary.a.white28@gmail.com>
2021-11-01 20:47:16 -0400

    completed movie exercise without bonus

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -2,6 +2,7 @@ package movies;
 
 import util.Input;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MoviesApplication {
 
@@ -11,30 +12,68 @@ public class MoviesApplication {
 //            System.out.println(movie.getMovieName());
 //        }
 
-        Movie[] searchResult = new Movie[0];
-        for(Movie movie : library){
-            if(movie.getMovieCategory().equalsIgnoreCase("drama")){
-               searchResult= Arrays.copyOf(searchResult, searchResult.length + 1);
-               searchResult[searchResult.length - 1] = movie;
-            }
-        }
-        System.out.println(library.length);
-        System.out.println(searchResult.length);
-        for(Movie movie : searchResult){
-                System.out.println(movie.getMovieName() + " - " + movie.getMovieCategory());
-            }
+        Movie[] searchResult = new Movie[0];    //this is a new Movie array called searchResult
+//        for(Movie movie : library){
+//            if(movie.getMovieCategory().equalsIgnoreCase("drama")){
+//               searchResult= Arrays.copyOf(searchResult, searchResult.length + 1);
+//               searchResult[searchResult.length - 1] = movie;
+//            }
+//        }
+//        for(Movie movie : searchResult){
+//                System.out.println(movie.getMovieName() + " - " + movie.getMovieCategory());
+//            }
 
         Input input = new Input();              //import my input class that I created last week
-        System.out.println("What would you like to do?");
-        System.out.println(" 0 - exit\n 1 - view all movies\n 2 - view movies in the animated category\n 3 - view movies in the drama category\n 4 - view movies in the horror category\n 5 - view movies in the scifi category\n");
-        System.out.print("Enter your choice: ");
-        int userChoice = input.getInt();
-        System.out.println(userChoice);
-        if(userChoice == 1){
-            for(Movie movie : library){
-                System.out.println(movie.getMovieName() + " - " + movie.getMovieCategory());
+        Scanner scanner = new Scanner(System.in);
+
+        String userReply;
+        OUTER:
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println(" 0 - exit\n 1 - view all movies\n 2 - view movies in the animated category\n 3 - view movies in the drama category\n 4 - view movies in the horror category\n 5 - view movies in the scifi category\n 6 - view movies in the musical category\n");
+            System.out.print("Enter your choice: ");
+            int userChoice = input.getInt();
+            System.out.println("You selected # " + userChoice);
+            for (Movie movie : library) {
+                if (userChoice == 1) {
+                    System.out.println("\t" + movie.getMovieName() + " - " + movie.getMovieCategory());
+                }
+                if (userChoice == 2) {
+                    //show movies in the category of animated
+                    if (movie.getMovieCategory().equalsIgnoreCase("animated")) {
+                        System.out.println("\t" + movie.getMovieName() + "--" + movie.getMovieCategory());
+                    }
+                }
+                if (userChoice == 3) {
+                    if (movie.getMovieCategory().equalsIgnoreCase("drama")) {
+                        System.out.println("\t" + movie.getMovieName() + "--" + movie.getMovieCategory());
+                    }
+                }
+                if (userChoice == 4) {
+                    if (movie.getMovieCategory().equalsIgnoreCase("horror")) {
+                        System.out.println("\t" + movie.getMovieName() + "--" + movie.getMovieCategory());
+                    }
+                }
+                if (userChoice == 5) {
+                    if (movie.getMovieCategory().equalsIgnoreCase("scifi")) {
+                        System.out.println("\t" + movie.getMovieName() + "--" + movie.getMovieCategory());
+                    }
+                }
+                if (userChoice == 6) {
+                    if (movie.getMovieCategory().equalsIgnoreCase("musical")) {
+                        System.out.println("\t" + movie.getMovieName() + "--" + movie.getMovieCategory());
+                    }
+                }
+                if (userChoice == 0) {
+                    System.out.println("###########");
+                    System.out.println("# Goodbye #");
+                    System.out.println("###########");
+                    break OUTER;
+                }
             }
-        }
+            System.out.println("Would you like to make another selection? Y for yes, N to exit");
+            userReply = scanner.nextLine();
+        }while(userReply.equalsIgnoreCase("Y"));
 
     }
 
