12660c6e9fea591c331f96c95cf87ec177ca91d1
Mary White <mary.a.white28@gmail.com>
2021-11-03 16:53:46 -0400

    fixed several incorrect values like jack/10 in the array

--- src/cardgames/CardsArray.java
+++ src/cardgames/CardsArray.java
@@ -13,10 +13,11 @@ public class CardsArray {
                 new Card("hearts", (short) 7, "7"),
                 new Card("hearts", (short) 8, "8"),
                 new Card("hearts", (short) 9, "9"),
-                new Card("hearts", (short) 10, "jack"), //jack
-                new Card("hearts", (short) 11, "queen"), //queen
-                new Card("hearts", (short) 12, "king"), //king
-                new Card("hearts", (short) 13, "ace"), //ace
+                new Card("hearts", (short) 10, "10"),
+                new Card("hearts", (short) 11, "Jack"),         //jack
+                new Card("hearts", (short) 12, "Queen"),        //queen
+                new Card("hearts", (short) 13, "King"),         //king
+                new Card("hearts", (short) 14, "Ace"),         //ace
                 new Card("diamonds", (short) 2, "2"),
                 new Card("diamonds", (short) 3, "3"),
                 new Card("diamonds", (short) 4, "4"),
@@ -25,11 +26,12 @@ public class CardsArray {
                 new Card("diamonds", (short) 7, "7"),
                 new Card("diamonds", (short) 8, "8"),
                 new Card("diamonds", (short) 9, "9"),
-                new Card("diamonds", (short) 10, "jack"), //jack
-                new Card("diamonds", (short) 11, "queen"), //queen
-                new Card("diamonds", (short) 12, "king"), //king
-                new Card("diamonds", (short) 13, "ace"), //ace
-                new Card("spades", (short) 3, "2"),
+                new Card("diamonds", (short) 10, "10"),
+                new Card("diamonds", (short) 11, "Jack"),    //jack
+                new Card("diamonds", (short) 12, "Queen"),   //queen
+                new Card("diamonds", (short) 13, "King"),    //king
+                new Card("diamonds", (short) 14, "Ace"),    //ace
+                new Card("spades", (short) 2, "2"),
                 new Card("spades", (short) 3, "3"),
                 new Card("spades", (short) 4, "4"),
                 new Card("spades", (short) 5, "5"),
@@ -37,22 +39,24 @@ public class CardsArray {
                 new Card("spades", (short) 7, "7"),
                 new Card("spades", (short) 8, "8"),
                 new Card("spades", (short) 9, "9"),
-                new Card("spades", (short) 10, "jack"), //jack
-                new Card("spades", (short) 11, "queen"), //queen
-                new Card("spades", (short) 12, "king"), //king
-                new Card("spades", (short) 13, "ace"), //ace
-                new Card("clubs", (short) 5, "2"),
-                new Card("clubs", (short) 5, "3"),
-                new Card("clubs", (short) 5, "4"),
+                new Card("spades", (short) 10, "10"),
+                new Card("spades", (short) 11, "Jack"),     //jack
+                new Card("spades", (short) 12, "Queen"),    //queen
+                new Card("spades", (short) 13, "King"),    //king
+                new Card("spades", (short) 14, "Ace"),     //ace
+                new Card("clubs", (short) 2, "2"),
+                new Card("clubs", (short) 3, "3"),
+                new Card("clubs", (short) 4, "4"),
                 new Card("clubs", (short) 5, "5"),
                 new Card("clubs", (short) 6, "6"),
                 new Card("clubs", (short) 7, "7"),
                 new Card("clubs", (short) 8, "8"),
                 new Card("clubs", (short) 9, "9"),
-                new Card("clubs", (short) 10, "jack"), //jack
-                new Card("clubs", (short) 11, "queen"), //queen
-                new Card("clubs", (short) 12, "king"), //king
-                new Card("clubs", (short) 13, "ace"), //ace
+                new Card("clubs", (short) 10, "10"),
+                new Card("clubs", (short) 11, "Jack"),      //jack
+                new Card("clubs", (short) 12, "Queen"),     //queen
+                new Card("clubs", (short) 13, "King"),     //king
+                new Card("clubs", (short) 14, "Ace"),      //ace
         };
     }
     }
