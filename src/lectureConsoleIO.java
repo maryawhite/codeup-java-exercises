4a16bc97b9dc65e5a89bcf38bb5307f66e0a84b8
Mary White <mary.a.white28@gmail.com>
2021-11-04 20:16:34 -0400

    add a while loop, do while loop, and for loop

--- src/lectureConsoleIO.java
+++ src/lectureConsoleIO.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class lectureConsoleIO {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -23,23 +23,23 @@ public class lectureConsoleIO {
 //        System.out.printf("2 out of 10 is %d%%", 20);  //what is percent d?
 //
 //        System.out.println();
-
-        System.out.print("Hello ");  //print prints everything on one line--the cursor remains at the end of the text at the console
-        System.out.print("World ");
-        System.out.println("Goodbye");  //println prints the next word on a new line--the cursor goes to the next line
-        System.out.println();           //println will work with or without a parameter
-        System.out.println("World");
-
-        boolean isSunny = false;
-        boolean hasUmbrella = true;
-
-        if(isSunny){
-            System.out.println("Enjoy the sunshine - don't forget to wear sunscreen");
-        } else if (!isSunny && hasUmbrella){
-            System.out.println("good job remembering your umbrella");
-        } else {
-            System.out.println("Go check the weather");
-        }
+//
+//        System.out.print("Hello ");  //print prints everything on one line--the cursor remains at the end of the text at the console
+//        System.out.print("World ");
+//        System.out.println("Goodbye");  //println prints the next word on a new line--the cursor goes to the next line
+//        System.out.println();           //println will work with or without a parameter
+//        System.out.println("World");
+//
+//        boolean isSunny = false;
+//        boolean hasUmbrella = true;
+//
+//        if(isSunny){
+//            System.out.println("Enjoy the sunshine - don't forget to wear sunscreen");
+//        } else if (!isSunny && hasUmbrella){
+//            System.out.println("good job remembering your umbrella");
+//        } else {
+//            System.out.println("Go check the weather");
+//        }
 //
 //        int caseSwitch = 1;
 //
@@ -109,27 +109,60 @@ public class lectureConsoleIO {
 //                System.out.println("idk");
 //                break;
 //        }
-        String reply;
-        do {
-            System.out.println("Enter any string: ");
-            String origString = scanner.nextLine();
-            String reverseString = "";              //initialize the string
+//        String reply;
+//        do {
+//            System.out.println("Enter any string: ");
+//            String origString = scanner.nextLine();
+//            String reverseString = "";              //initialize the string
+//
+//            char[] characters = origString.toCharArray();  //the toCharArray method takes the string and turns it into an array of chars
+//
+//            for (int i = characters.length - 1; i >= 0; i--) {     //this is starting at the end and decrementing
+//                reverseString = reverseString + characters[i];
+//            }
+//
+//            if (origString.equals(reverseString)) {
+//                System.out.println("String is a palindrome");
+//            } else {
+//                System.out.println("String is not a palindrome");
+//            }
+//            System.out.println("Would you like to enter another work? enter y or n");
+//            reply = scanner.nextLine();
+//        }while(reply.equals("y"));
 
-            char[] characters = origString.toCharArray();  //the toCharArray method takes the string and turns it into an array of chars
+        //create a while loop that counts to 10
+//        int counter = 0;
+//        while(counter < 10){
+//            counter += 1;
+//            System.out.println(counter);
+//        }
+        whileLoop();
+        doWhileLoop();
+        forLoop();
 
-            for (int i = characters.length - 1; i >= 0; i--) {     //this is starting at the end and decrementing
-                reverseString = reverseString + characters[i];
-            }
+    }   //end of psvm
 
-            if (origString.equals(reverseString)) {
-                System.out.println("String is a palindrome");
-            } else {
-                System.out.println("String is not a palindrome");
-            }
-            System.out.println("Would you like to enter another work? enter y or n");
-            reply = scanner.nextLine();
-        }while(reply.equals("y"));
-
+    static void whileLoop() {
+        int counter = 0;
+        while (counter < 10) {
+            counter += 1;
+            System.out.println("whileLoop: " + counter);
+        }
     }
 
+    static void doWhileLoop() {
+        int counter = 10;
+        do {
+            counter += 1;
+            System.out.println("doWhileLoop: " + counter);
+
+        } while (counter < 10);
+    }
+
+    static void forLoop(){    //note that void methods do not have a return statement
+        for(int counter = 0; counter < 10; counter += 1){
+            System.out.println("forLoop: " + counter);
+        }
+    }
 }
+
