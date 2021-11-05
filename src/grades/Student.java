eed9f7c46c82382ffca011cbdfe5fb8579fe8ccd
Mary White <mary.a.white28@gmail.com>
2021-11-05 13:11:58 -0400

    completed part 1 of the exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -9,7 +9,7 @@ public class Student {
     //a constructor to set the name property and initialize the grades property as an empty ArrayList
     public Student(String name, ArrayList<Integer> grades) {
         this.name = name;
-        this.grades = new ArrayList<>();
+        this.grades = grades;
     }
 
     //methods
@@ -23,12 +23,35 @@ public class Student {
 
     public double getGradeAverage(){
         Integer sum = 0;
+        if(grades.isEmpty()){
+            return 0;  //how do I prevent it from dividing by zero?
+        }
         for(Integer oneGrade : grades){
-            if(!grades.isEmpty()){
                 sum += oneGrade;
-            }
         }
         return sum/grades.size();
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> grades = new ArrayList<>();  //initalize a new grades arrayList
+        Student mary = new Student("Mary", grades);
+        mary.addGrade(100);
+        mary.addGrade(80);
+        System.out.println(grades);
+        System.out.println(mary.getName());
+        System.out.println("mary.getGradeAverage() = " + mary.getGradeAverage());
+        ArrayList<Integer> grades2 = new ArrayList<>();
+        Student kevin = new Student("Kevin", grades2);
+        kevin.addGrade(100);
+        kevin.addGrade(90);
+        System.out.println("kevin.getGradeAverage() = " + kevin.getGradeAverage());
+        ArrayList<Integer> grades3 = new ArrayList<>();
+        Student steven = new Student("Steven", grades3);
+        steven.addGrade(90);
+        steven.addGrade(85);
+        System.out.println("steven.getGradeAverage() = " + steven.getGradeAverage());
+
     }
 
 }
