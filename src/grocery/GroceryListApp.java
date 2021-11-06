05c8f20a4da5c315df12fe7c97f48883347fce9b
Mary White <mary.a.white28@gmail.com>
2021-11-05 22:16:05 -0400

    added more hashmaps, bug-prompting twice to continue

--- src/grocery/GroceryListApp.java
+++ src/grocery/GroceryListApp.java
@@ -2,21 +2,96 @@ package grocery;
 
 import util.Input;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GroceryListApp {
 
     public static void main(String[] args) {
-        //import my input class
         Input input = new Input();
+
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("1. Dairy");
+        categories.add("2. Produce");
+        categories.add("3. Frozen");
+        categories.add("4. Bakery");
+        categories.add("5. Deli");
+        categories.add("6. Meat/Seafood");
+        categories.add("7. Other");
+
+        //make a hash map for each category
+        HashMap<String, Integer> dairyMap = new HashMap<>();
+        HashMap<String, Integer> produceMap = new HashMap<>();
+        HashMap<String, Integer> frozenMap = new HashMap<>();
+        HashMap<String, Integer> bakeryMap = new HashMap<>();
+        HashMap<String, Integer> deliMap = new HashMap<>();
+        HashMap<String, Integer> meatSeafoodMap = new HashMap<>();
+        HashMap<String, Integer> otherMap = new HashMap<>();
+
+        //first prompt:
         System.out.println("Hello, would you like to create a grocery list? y for yes, n for n");
-        if(input.yesNo()){
+        OUTER:
+        do {
+            if (input.yesNo()) {      //this pulls up scanner.nextLine(); if y or yes it returns true
+                System.out.println("Please enter a number for the category: ");
+                System.out.println(categories);
+                int userEntry = input.getInt();  //input.getInt returns scanner.nextInt()
+                input.getString();             //clear out the scanner
 
-        }
-        //If the users picks yes, they should be given the following three prompts:
-        //Given an ordered list of grocery categories to choose from, select the category.
-        //Enter the name of the item.
-        //Enter how many of the item.
+                do {
+                    if (userEntry == 1) {
+                        System.out.println("Enter your item for the category of Dairy: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        dairyMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 2) {
+                        System.out.println("Enter your item for the category of Produce: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        produceMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 3) {
+                        System.out.println("Enter your item for the category of Frozen: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        frozenMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 4) {
+                        System.out.println("Enter your item for the category of Bakery: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        bakeryMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 5) {
+                        System.out.println("Enter your item for the category of Deli: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        deliMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 6) {
+                        System.out.println("Enter your item for the category of Meat/Seafood: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        meatSeafoodMap.put(userItemEntry, userQty);
+                    } else if (userEntry == 7) {
+                        System.out.println("Enter your item for the category of Other: ");
+                        String userItemEntry = input.getString();
+                        System.out.println("Enter Quantity");
+                        int userQty = input.getInt();
+                        otherMap.put(userItemEntry, userQty);
+                    } else {
+                        System.out.println("Invalid response");
+                        break OUTER;
+                    }
+                    input.getString(); //clear out the scanner?  //this one prevents it from asking you 2 questions
 
-
-
+                    System.out.println("Do you want to add another item in this same category. Y/N");
+                } while (input.yesNo());    ////this pulls up scanner.nextLine(); if y or yes it returns true
+            }
+            System.out.println("Do you want to continue. Y/N");
+        }while(input.yesNo());
+        System.out.println("Here's your list " + dairyMap + produceMap + frozenMap + bakeryMap + deliMap + meatSeafoodMap + otherMap);
     }
 }
