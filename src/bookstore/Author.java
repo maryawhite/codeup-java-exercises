6c72d785bbc5bc817f5fa30df3266f8eec5aa42e
Mary White <mary.a.white28@gmail.com>
2021-11-08 18:58:44 -0500

    still working on author

--- src/bookstore/Author.java
+++ src/bookstore/Author.java
@@ -3,10 +3,17 @@ package bookstore;
 public class Author extends Person {
     //- Make an Author subclass that inherits from person,
     // add a counter for the number of books written
+    private String name;
     private int counter;
 
     public Author(String name){
         super(name);
     }
+
+    public String getAuthorName(String name){
+        return this.name;
+    }
+
+
 
 }
